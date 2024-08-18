73619d5b94c668448891a73bcd5ac5c675115717
SL55ZY <65545295+profsleezy@users.noreply.github.com>
2024-08-18 18:28:54 +0300

    s

--- src/games/ExampleGame/index.tsx
+++ src/games/ExampleGame/index.tsx
@@ -1,116 +1,76 @@
-import { Canvas } from '@react-three/fiber'
-import { GambaUi, useSound } from 'gamba-react-ui-v2'
-import { useGamba } from 'gamba-react-v2'
+import { GambaUi, useSound, useWagerInput } from 'gamba-react-ui-v2'
 import React from 'react'
-import SOUND_LOSE from './lose.mp3'
-import SOUND_WIN from './win.mp3'
+import SOUND from './test.mp3'
 
-const SIDES = {
-  black: [2, 0],
-  white: [0, 2],
-}
-const WAGER_OPTIONS = [1, 5, 10, 50, 100]
-
-type Side = keyof typeof SIDES
-
-function CockFight() {
+export default function ExampleGame() {
+  const _hue = React.useRef(0)
+  const [wager, setWager] = useWagerInput()
   const game = GambaUi.useGame()
-  const gamba = useGamba()
-  const [fighting, setFighting] = React.useState(false)
-  const [win, setWin] = React.useState(false)
-  const [resultIndex, setResultIndex] = React.useState(0)
-  const [side, setSide] = React.useState<Side>('black')
-  const [wager, setWager] = React.useState(WAGER_OPTIONS[0])
+  const sound = useSound({ test: SOUND })
 
-  const sounds = useSound({
-    win: SOUND_WIN,
-    lose: SOUND_LOSE,
-  })
+  const click = () => {
+    _hue.current = (_hue.current + 30) % 360
+    sound.play('test', { playbackRate: .75 + Math.random() * .5 })
+  }
 
   const play = async () => {
-    try {
-      setWin(false)
-      setFighting(true)
-
-      await game.play({
-        bet: SIDES[side],
-        wager,
-        metadata: [side],
-      })
-
-      sounds.play('rooster')
-
-      const result = await game.result()
-
-      const win = result.payout > 0
-
-      setResultIndex(result.resultIndex)
-
-      setWin(win)
-
-      if (win) {
-        sounds.play('win')
-      } else {
-        sounds.play('lose')
-      }
-    } finally {
-      setFighting(false)
-    }
+    await game.play({
+      wager,
+      bet: [2, 0],
+    })
+    const result = await game.result()
+    console.log(result)
   }
 
   return (
     <>
       <GambaUi.Portal target="screen">
-        <Canvas
-          linear
-          flat
-          orthographic
-          camera={{
-            zoom: 80,
-            position: [0, 0, 100],
-          }}
-        >
-          <React.Suspense fallback={null}>
-            <Cock result={resultIndex} fighting={fighting} />
-          </React.Suspense>
-          <Effect color="white" />
+        <GambaUi.Canvas
+          render={({ ctx, size }, clock) => {
+            const scale = 3 + Math.cos(clock.time) * .5
+            const hue = _hue.current
 
-          {fighting && <Effect color="white" />}
-          {win && <Effect color="#42ff78" />}
-          <ambientLight intensity={3} />
-          <directionalLight
-            position-z={1}
-            position-y={1}
-            castShadow
-            color="#CCCCCC"
-          />
-          <hemisphereLight
-            intensity={.5}
-            position={[0, 1, 0]}
-            scale={[1, 1, 1]}
-            color="#ffadad"
-            groundColor="#6666fe"
-          />
-        </Canvas>
+            ctx.fillStyle = 'hsla(' + hue + ', 50%, 3%, 1)'
+            ctx.fillRect(0, 0, size.width, size.height)
+
+            ctx.save()
+            ctx.translate(size.width / 2, size.height / 2)
+
+            for (let i = 0; i < 5; i++) {
+              ctx.save()
+              ctx.scale(scale * (1 + i), scale * (1 + i))
+              ctx.fillStyle = 'hsla(' + hue + ', 75%, 60%, .2)'
+              ctx.beginPath()
+              ctx.arc(0, 0, 10, 0, Math.PI * 2)
+              ctx.fill()
+              ctx.restore()
+            }
+
+            ctx.fillStyle = 'hsla(' + hue + ', 75%, 60%, 1)'
+            ctx.beginPath()
+            ctx.arc(0, 0, 8, 0, Math.PI * 2)
+            ctx.fill()
+
+            ctx.textAlign = 'center'
+            ctx.textBaseline = 'middle'
+            ctx.font = '32px Arial'
+
+            ctx.fillStyle = 'hsla(' + hue + ', 75%, 90%, 1)'
+            ctx.fillText('HELLO', 0, 0)
+
+            ctx.restore()
+          }}
+        />
       </GambaUi.Portal>
       <GambaUi.Portal target="controls">
-        <GambaUi.WagerInput
-          options={WAGER_OPTIONS}
-          value={wager}
-          onChange={setWager}
-        />
-        <GambaUi.Button disabled={gamba.isPlaying} onClick={() => setSide(side === 'black' ? 'white' : 'black')}>
-          <div style={{ display: 'flex' }}>
-            <img height="20px" src={side === 'black' ? 'black_cock.png' : 'white_cock.png'} />
-            {side === 'black' ? 'Black Cock' : 'White Cock' }
-          </div>
+        <GambaUi.WagerInput value={wager} onChange={setWager} />
+        <GambaUi.Button onClick={click}>
+          Useless button
         </GambaUi.Button>
         <GambaUi.PlayButton onClick={play}>
-          Fight!
+          Double Or nothing
         </GambaUi.PlayButton>
       </GambaUi.Portal>
     </>
   )
 }
-
-export default CockFight
