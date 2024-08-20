accb848c0183a4c69ee4abbc555c5fd1fd508a4e
SL55ZY <65545295+profsleezy@users.noreply.github.com>
2024-08-20 21:00:29 +0300

    yes

--- src/sections/RecentPlays/RecentPlays.tsx
+++ src/sections/RecentPlays/RecentPlays.tsx
@@ -90,9 +90,6 @@ export default function RecentPlays() {
           </Recent>
         ),
       )}
-      <GambaUi.Button main onClick={() => window.open(`${EXPLORER_URL}/platform/${PLATFORM_CREATOR_ADDRESS.toString()}`)}>
-        🚀 Explorer
-      </GambaUi.Button>
     </Container>
   )
 }
