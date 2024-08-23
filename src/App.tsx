9beaed26421d9552a50c5494bc2d2ff32a3f582c
SL55ZY <65545295+profsleezy@users.noreply.github.com>
2024-08-23 23:47:25 +0300

    try

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ import { Modal } from './components/Modal'
 import { useToast } from './hooks/useToast'
 import { useUserStore } from './hooks/useUserStore'
 import Dashboard from './sections/Dashboard/Dashboard'
-import Game from './games/ExampleGame/index'
+import Game from './sections/Game/Game'
 import Header from './sections/Header'
 import RecentPlays from './sections/RecentPlays/RecentPlays'
 import Toasts from './sections/Toasts'
@@ -72,7 +72,7 @@ export default function App() {
       <Toasts />
       <MainWrapper>
         <Routes>
-          <Route path="/" element={<Game />} />
+          <Route path="/:gameId" element={<Game />} />
         </Routes>
         <h2 style={{ textAlign: 'center' }}>Recent Plays</h2>
         <RecentPlays />
