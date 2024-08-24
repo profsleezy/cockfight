5eccbb047efc7fa31a1e3703f3da81287622b135
SL55ZY <65545295+profsleezy@users.noreply.github.com>
2024-08-24 18:00:56 +0300

    committ

--- src/games/ExampleGame/BottomIcons.tsx
+++ src/games/ExampleGame/BottomIcons.tsx
@@ -1,8 +1,8 @@
 // BottomIcons.tsx
 import React from 'react';
-import { ReactComponent as Icon1 } from './icon1.svg';
-import { ReactComponent as Icon2 } from './icon2.svg';
-import { ReactComponent as Icon3 } from './icon3.svg';
+import Icon1 from './icon1.svg';
+import Icon2 from './icon2.svg';
+import Icon3 from './icon3.svg';
 import './BottomIcons.css'; // Import the CSS for styling
 
 const BottomIcons: React.FC = () => {
