afdc99c8419e085736507181f1eb2a56905eb1a3
SL55ZY <65545295+profsleezy@users.noreply.github.com>
2024-08-24 17:57:49 +0300

    TRIAL

--- src/games/ExampleGame/index.tsx
+++ src/games/ExampleGame/index.tsx
@@ -5,9 +5,7 @@ import WIN_SOUND from './win.mp3';
 import LOSS_SOUND from './lose.mp3';
 import chicken1 from './gif1.png'; // Black cock
 import chicken2 from './gif2.png'; // White cock
-import { ReactComponent as Icon1 } from './icon1.svg';
-import Icon2 from './icon2.svg';
-import Icon3 from './icon3.svg';
+import BottomIcons from './BottomIcons';
 
 
  const SIDES = {
