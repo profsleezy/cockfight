2a5b66f4bc32f2a7045d648912389638d48a97b0
SL55ZY <65545295+profsleezy@users.noreply.github.com>
2024-08-24 18:11:57 +0300

    please

--- src/games/ExampleGame/index.tsx
+++ src/games/ExampleGame/index.tsx
@@ -5,7 +5,7 @@ import WIN_SOUND from './win.mp3';
 import LOSS_SOUND from './lose.mp3';
 import chicken1 from './gif1.png'; // Black cock
 import chicken2 from './gif2.png'; // White cock
-import { BottomIcons } from './BottomIcons';
+import BottomIcons from './BottomIcons';
 
 
  const SIDES = {
