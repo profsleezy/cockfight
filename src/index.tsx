d19bb3d8ab82c8db8459225d69e95812d357d0d9
SL55ZY <65545295+profsleezy@users.noreply.github.com>
2024-08-25 11:21:08 +0300

    try

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,12 @@
+import React, { useEffect } from 'react'
+import ReactDOM from 'react-dom/client'
+import { BrowserRouter, useNavigate } from 'react-router-dom'
 import { ConnectionProvider, WalletProvider } from '@solana/wallet-adapter-react'
 import { WalletModalProvider } from '@solana/wallet-adapter-react-ui'
 import '@solana/wallet-adapter-react-ui/styles.css'
 import { PhantomWalletAdapter, SolflareWalletAdapter } from '@solana/wallet-adapter-wallets'
 import { GambaPlatformProvider, TokenMetaProvider } from 'gamba-react-ui-v2'
 import { GambaProvider, SendTransactionProvider } from 'gamba-react-v2'
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
 import App from './App'
 import { DEFAULT_POOL, PLATFORM_CREATOR_ADDRESS, PLATFORM_CREATOR_FEE, PLATFORM_JACKPOT_FEE, RPC_ENDPOINT, TOKEN_METADATA, TOKEN_METADATA_FETCHER } from './constants'
 import './styles.css'
@@ -22,41 +22,51 @@ function Root() {
     [],
   )
 
+  const navigate = useNavigate()
+
+  useEffect(() => {
+    if (window.location.pathname !== '/cockfight') {
+      navigate('/cockfight')
+    }
+  }, [navigate])
+
   return (
-    <BrowserRouter>
-      <ConnectionProvider
-        endpoint={RPC_ENDPOINT}
-        config={{ commitment: 'processed' }}
-      >
-        <WalletProvider autoConnect wallets={wallets}>
-          <WalletModalProvider>
-            <TokenMetaProvider
-              tokens={TOKEN_METADATA}
-              fetcher={TOKEN_METADATA_FETCHER}
-            >
-              <SendTransactionProvider priorityFee={400_201}>
-                <GambaProvider
-                  __experimental_plugins={[
-                    // Custom fee (1%)
-                    // createCustomFeePlugin('<SOLANA ADDRESS>', .01),
-                  ]}
+    <ConnectionProvider
+      endpoint={RPC_ENDPOINT}
+      config={{ commitment: 'processed' }}
+    >
+      <WalletProvider autoConnect wallets={wallets}>
+        <WalletModalProvider>
+          <TokenMetaProvider
+            tokens={TOKEN_METADATA}
+            fetcher={TOKEN_METADATA_FETCHER}
+          >
+            <SendTransactionProvider priorityFee={400_201}>
+              <GambaProvider
+                __experimental_plugins={[
+                  // Custom fee (1%)
+                  // createCustomFeePlugin('<SOLANA ADDRESS>', .01),
+                ]}
+              >
+                <GambaPlatformProvider
+                  creator={PLATFORM_CREATOR_ADDRESS}
+                  defaultCreatorFee={PLATFORM_CREATOR_FEE}
+                  defaultJackpotFee={PLATFORM_JACKPOT_FEE}
+                  defaultPool={DEFAULT_POOL}
                 >
-                  <GambaPlatformProvider
-                    creator={PLATFORM_CREATOR_ADDRESS}
-                    defaultCreatorFee={PLATFORM_CREATOR_FEE}
-                    defaultJackpotFee={PLATFORM_JACKPOT_FEE}
-                    defaultPool={DEFAULT_POOL}
-                  >
-                    <App />
-                  </GambaPlatformProvider>
-                </GambaProvider>
-              </SendTransactionProvider>
-            </TokenMetaProvider>
-          </WalletModalProvider>
-        </WalletProvider>
-      </ConnectionProvider>
-    </BrowserRouter>
+                  <App />
+                </GambaPlatformProvider>
+              </GambaProvider>
+            </SendTransactionProvider>
+          </TokenMetaProvider>
+        </WalletModalProvider>
+      </WalletProvider>
+    </ConnectionProvider>
   )
 }
 
-root.render(<Root />)
+root.render(
+  <BrowserRouter>
+    <Root />
+  </BrowserRouter>
+)
