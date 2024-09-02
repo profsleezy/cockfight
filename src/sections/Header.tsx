f4141f73a5cffaababb9b22b7533920a57ca00c7
SL55ZY <65545295+profsleezy@users.noreply.github.com>
2024-09-02 06:38:44 +0300

    header update #11

--- src/sections/Header.tsx
+++ src/sections/Header.tsx
@@ -28,7 +28,7 @@ const AirdropButton = styled.button`
   all: unset;
   cursor: pointer;
   color: black;
-  border-radius: 5px;
+  border-radius: 2px;
   background: #E8A63A;
   padding: 10px;
   font-size: 20px;
@@ -38,14 +38,14 @@ const AirdropButton = styled.button`
   &:hover::after {
     content: 'Coming Soon';
     position: absolute;
-    bottom: -25px;
+    bottom: -28px;
     left: 50%;
     transform: translateX(-50%);
     background: rgba(0, 0, 0, 0.8);
     color: white;
     padding: 10px;
-    border-radius: 5px;
-    font-size: 10px;
+    border-radius: 3px;
+    font-size: 15px;
     white-space: nowrap;
     z-index: 1000;
   }
@@ -103,8 +103,7 @@ const Label = styled.span`
   /* Adjusted positioning to stay fixed */
   left: 10px; /* For 'Black Cock' */
   &:last-child {
-    right: 10px; /* For 'White Cock' */
-    left: auto;
+    left: 10px; /* For 'White Cock' */
   }
 `;
 
